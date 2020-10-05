a0a06a3ea212b61118b9da146a89263e91285ebb
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-05 11:17:53 +0300

    Solution B to palindrome check problem

--- palindromeCheck.js
+++ palindromeCheck.js
@@ -13,3 +13,23 @@ const solutionA = (str) => {
 
   return str === reversedStrArray.join('');
 };
+
+/**
+ *
+ * @param {String} str
+ *
+ * Time O(n)
+ * Space O(n)
+ */
+const solutionB = (str) => {
+  return isPalindromeRecursion(str);
+};
+
+const isPalindromeRecursion = (str, firstCharacterIndex = 0) => {
+  const lastCharacterIndex = str.length - 1 - firstCharacterIndex;
+  if (firstCharacterIndex >= lastCharacterIndex) {
+    return true;
+  } else {
+    return str[firstCharacterIndex] === str[lastCharacterIndex] && isPalindromeRecursion(str, firstCharacterIndex + 1);
+  }
+};
