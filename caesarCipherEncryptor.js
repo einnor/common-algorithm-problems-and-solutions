f28d9901dfc5ed62b74a66e745a9a964adb0cde8
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-05 15:13:19 +0300

    Solution B to caesar cipher encryptor problem

--- caesarCipherEncryptor.js
+++ caesarCipherEncryptor.js
@@ -19,9 +19,36 @@ const solutionA = (str, key) => {
 
 const getNewLetter = (letter, key) => {
   const newLetterCode = letter.charCodeAt(0) + key;
-  console.log(newLetterCode);
   if (newLetterCode <= 122) {
     return String.fromCharCode(newLetterCode);
   }
   return String.fromCharCode(96 + newLetterCode % 122);
+};
+
+/**
+ *
+ * @param {String} str
+ * @param {Number} key
+ *
+ * Time O(n)
+ * Space O(n)
+ */
+const solutionB = (str, key) => {
+  const newLetters = [];
+  const newKey = key % 26;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  for (const letter of str) {
+    const newLetter = getNewLetterFromAlphabet(letter, newKey, alphabet);
+    newLetters.push(newLetter);
+  }
+
+  return newLetters.join('');
+};
+
+const getNewLetterFromAlphabet = (letter, key, alphabet) => {
+  const newLetterCode = alphabet.indexOf(letter) + key;
+  if (newLetterCode <= 25) {
+    return alphabet[newLetterCode];
+  }
+  return alphabet[-1 + newLetterCode % 25];
 };
