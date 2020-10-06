066d6b89d46503b960a66f93f28776c72c943316
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-06 14:17:42 +0300

    Solution to in order traversal of a bst problem

---
+++ binarySearchTreeTraversal.js
@@ -0,0 +1,8 @@
+const inOrderTraverse = (tree, array) => {
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array);
+  }
+  return array;
+};
