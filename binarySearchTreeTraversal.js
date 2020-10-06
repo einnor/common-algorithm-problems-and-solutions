fa6f333726979b15a76db482984ca77ad086d5ee
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-06 14:22:10 +0300

    Solution to post order traversal of a bst problem

--- binarySearchTreeTraversal.js
+++ binarySearchTreeTraversal.js
@@ -1,3 +1,11 @@
+/**
+ *
+ * @param {BST} tree
+ * @param {Array} array
+ *
+ * Time O(n)
+ * Space O(n) because we are storing everything in an array
+ */
 const inOrderTraverse = (tree, array) => {
   if (tree !== null) {
     inOrderTraverse(tree.left, array);
@@ -7,11 +15,36 @@ const inOrderTraverse = (tree, array) => {
   return array;
 };
 
+/**
+ *
+ * @param {BST} tree
+ * @param {Array} array
+ *
+ * Time O(n)
+ * Space O(n) because we are storing everything in an array
+ */
 const preOrderTraverse(tree, array) => {
   if (tree !== null) {
     array.push(tree.value);
     preOrderTraverse(tree.left, array);
     preOrderTraverse(tree.right, array);
+  }
+  return array;
+};
+
+/**
+ *
+ * @param {BST} tree
+ * @param {Array} array
+ *
+ * Time O(n)
+ * Space O(n) because we are storing everything in an array
+ */
+const postOrderTraverse(tree, array) => {
+  if (tree !== null) {
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value);
   }
   return array;
 };
