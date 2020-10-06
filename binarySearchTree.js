aa9e2fa04284bc90573c7f3a437a1674993c7e11
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-06 11:46:05 +0300

    Method to delete a node in the bst

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -54,4 +54,53 @@ class BST {
     }
     return false;
   };
+
+  remove(value, parentNode = null) {
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        // Searching left subtree
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        // Searching right subtree
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        // Matched
+        // Node has left and right children
+        if (currentNode.left !== null && currentNode.right !== null) {
+          // currentNode.value = smallest value of right subtree
+          currentNode.value = currentNode.right.getMinValue();
+          currentNode.right.remove(currentNode.value, currentNode);
+        } else if (parentNode === null) {
+          if (currentNode.left !==  null) {
+            currentNode.value = currentNode.left.value;
+            currentNode.right =  currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          } else if (currentNode.right !== null) {
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right =  currentNode.right.right;
+          } else {
+            currentNode.value = null;
+          }
+        } else if (parentNode.left === currentNode) {
+          if (currentNode.left !== null) {
+            parentNode.left = currentNode.left
+          } else {
+            parentNode.left = currentNode.right;
+          }
+        } else if (parentNode.right === currentNode) {
+          if (currentNode.left !== null) {
+            parentNode.right = currentNode.left
+          } else {
+            parentNode.right = currentNode.right;
+          }
+        }
+        break;
+      }
+    }
+    return this;
+  };
 };
