d246e90f98a9afd6b54ea66c001a46e57d5f3d0e
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-06 15:40:12 +0300

    Solution B to max subset sum no adjacent problem

--- maxSubsetSumNoAdjacent.js
+++ maxSubsetSumNoAdjacent.js
@@ -20,3 +20,28 @@ const solutionA = (array) => {
 
   return maxSums[maxSums.length - 1];
 };
+
+/**
+ *
+ * @param {Array of + Numbers} array
+ *
+ * Time O(n)
+ * Space O(1)
+ */
+const solutionB = (array) => {
+  if (!array.length) {
+    return;
+  }
+  if (array.length === 1) {
+    return array[0];
+  }
+  let second = array[0];
+  let first = Math.max(array[0], array[1]);
+  for (let i = 2; i < array.length; i++) {
+    const current = Math.max(first, second + array[i]);
+    second = first;
+    first = current;
+  }
+
+  return first;
+};
