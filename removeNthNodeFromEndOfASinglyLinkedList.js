5e6cf219be0d4895e2f659d8ec8daecaaecfd34f
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-07 11:04:47 +0300

    Solution to remove nth node from end of a singly linked list problem

---
+++ removeNthNodeFromEndOfASinglyLinkedList.js
@@ -0,0 +1,19 @@
+const solution = (head, n) => {
+  let counter = 1;
+  let first = head;
+  let second = head;
+  while (counter <= n) {
+    second = second.next;
+    counter += 1;
+  }
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+  }
+  while (second.next !== null) {
+    second = second.next;
+    first = first.next;
+  }
+  first.next = first.next.next;
+};
