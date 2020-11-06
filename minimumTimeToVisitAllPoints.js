e36156a4aa694b1077b5622d1bd5149fa4a881b5
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-11-06 14:54:51 +0300

    Recursive solution to minimum time taken to visit all points in a cartesian plane problem

--- minimumTimeToVisitAllPoints.js
+++ minimumTimeToVisitAllPoints.js
@@ -9,3 +9,17 @@ const solution = (points) => {
 
   return totalDistance;
 };
+
+const recursiveSolution = (points, xDistance = 0, yDistance = 0, totalDistance, index) => {
+  if (index === points.length - 1) {
+    return totalDistance;
+  }
+
+  return recursiveSolution(
+    points,
+    xDistance = Math.abs(points[index][0] - points[index + 1][0]),
+    yDistance = Math.abs(points[index][1] - points[index + 1][1]),
+    totalDistance = Math.max(xDistance, yDistance),
+    index + 1,
+  )
+};
