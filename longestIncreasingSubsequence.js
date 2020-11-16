54723561193b6bf22b61bcae7f160179ef96743e
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-11-16 15:52:06 +0300

    Solution B to the longest increasing subsequence problem

--- longestIncreasingSubsequence.js
+++ longestIncreasingSubsequence.js
@@ -39,3 +39,39 @@ const buildSequence = (array, sequences, currentIdx) => {
 
   return sequence.reverse();
 };
+
+/**
+ *
+ * @param {Array} array
+ *
+ * Time O(nlogn)
+ * Spacce O(n)
+ */
+const solutionA = (array) => {
+  const sequences = Array.from(Array(array.length).keys()).map(() => null);
+  const indices = Array.from(Array(array.length + 1).keys()).map(() => null);
+  let length = 0;
+
+  array.forEach((num, i) => {
+    const newLength = binarySearch(1, length, indices, array, num);
+    sequences[i] = indices[newLength - 1];
+    indices[newLength] = i;
+    length = Math.max(length, newLength);
+  });
+
+  return buildSequence(array, sequences, indices[length]);
+};
+
+const binarySearch = (startIdx, endIdx, indices, array, num) => {
+  if (startIdx > endIdx) {
+    return startIdx;
+  }
+
+  const middleIdx = Math.floor((startIdx + endIdx) / 2);
+  if (array[indices[middleIdx]] < num) {
+    startIdx = middleIdx + 1;
+  } else {
+    endIdx = middleIdx - 1;
+  }
+  return binarySearch(startIdx, endIdx, indices, num);
+};
