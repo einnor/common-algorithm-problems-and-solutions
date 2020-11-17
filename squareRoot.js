1e1edd875ef31a18e792c50e2aa334647e84a8f8
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-11-17 21:39:06 +0300

    Solution B to the square root problem

--- squareRoot.js
+++ squareRoot.js
@@ -20,3 +20,38 @@ const solutionA = (n) => {
 
   return i - 1;
 };
+
+/**
+ *
+ * @param {Integer} n
+ *
+ * Time O(logn)
+ * Space O(1)
+ */
+const solutionB = (n) => {
+  // Base cases
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  // Binary Search
+  let start = 1;
+  let end = n;
+  let answer;
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    // If n is a perfect square
+    if (mid * mid === n) {
+      return mid;
+    }
+
+    if (mid * mid < n) {
+      start = mid + 1;
+      answer = mid;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return answer;
+};
